1a1358dc80c76cff7eed1ac99f8848123ebe75b5
Glitchyi <advaithnarayanan8@gmail.com>
2022-06-07 21:55:45 +0530

    okay

--- multiplication.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-    int n;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    for (int i=1;i <= 10;i++){
-        printf("%d * %d = %d\n",i,n,i*n);
-    }
-}
