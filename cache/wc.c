6dad513313f1729fcafe9173e2ea32fd91df2d2a
Advaith Narayanan <55801439+Glitchyi@users.noreply.github.com>
2022-07-06 14:18:15 +0530

    Update wc.c

--- cache/wc.c
+++ cache/wc.c
@@ -1,16 +1,30 @@
 //Read a string ending with $ symbol and store it in an array and count the number of consonants and vowels and spaces in the string.
-void main(){
-    char ref[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    char str[100];
-    printf("Enter a string: ");
-    scanf("%s",str);
-    int j=0;
-    while(str[j]='\0'){
-    for (int i=0;i<26;i++){
-        printf("%c",ref[i]);
-        
-    }
+#include<stdio.h>
+void main()
+	{
+	int i,l=0,v=0,s=0,c=0;
+	char a[20];
+	printf("Enter the 1st string: ");
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+		{
+		if (a[i]=='a' ||a[i]=='e'||a[i]=='i'|a[i]=='o'||a[i]=='u')
+		{
+		v++;
+		}
+		else if(a[i]==' ')
+		{
+		s++;
+		}
+		else
+		{
+		c++;
+		}
+		
+	}
+	printf("the number of vowels %d\n",v);
+	printf("the number of spaces %d\n",s);
+	printf("the number of consonants %d\n",c);
+	}
 
-    }
-    
-}
+//credit : Ahkil Gopakumar
